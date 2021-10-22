607458552a8a7585464d4bc76bc3e035ec062c80
Anmay Gupta <anmaygupta1@gmail.com>
2021-10-21 19:44:40 -0500

    test for env variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
- 
+ require("dotenv").config({ path: "./config.env" });
+
 const app = express();
 
 app.use(cors())
@@ -11,6 +12,8 @@ app.use(function (rew, res, next) {
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next()
 })
+// get driver connection
+const dbo = require("./db/conn");
  
 app.get('/', (req, res) => {
    return res.send("This is a new thing")
