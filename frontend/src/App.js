1bed3a988a59bf8898a3e1c6c392e9771503eb21
Anmay Gupta <anmaygupta1@gmail.com>
2021-10-21 20:34:30 -0500

    added mongo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShopItem from './components/ShopItem.js';
 
 function App() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/page" />
             </Switch>
         </BrowserRouter>
     );
