1a774b09519c9ffa9983b1f523f8b73b92a783b2
sakshamgupta <catchgsaksham@gmail.com>
2021-10-24 19:13:40 -0500

    accounts started

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar({history}) {
     return (
         <div className="flex w-screen justify-between items-center pl-20 pr-20 pt-6 pb-6">
             <p className="text-xl font-semibold cursor-pointer" onClick={() => {history.push("/")}}>STOCKOLI</p>
-            <input type="text" className="bg-gray-200 rounded w-96 h-10 outline-none pl-4 pr-4 text-lg"/>
+            <input type="text" className="bg-white rounded w-2/5 h-10 outline-none pl-4 pr-4 text-lg" placeholder="Find items here"/>
             <div className="flex space-x-20 items-center">
                 <p className="cursor-pointer" onClick={() => {history.push("/aboutus")}}>ABOUT US</p>
                 <AccountCircleIcon fontSize="large" className="cursor-pointer" onClick={() => {history.push("/account")}}/>
--- frontend/src/components/cart/CartItem.js
+++ frontend/src/components/cart/CartItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CartItem = () => {
     return (
         <div className="flex h-20 space-x-4">
-            <div className="w-20 border-2 bg-gray-200 flex items-center rounded">
+            <div className="w-20 bg-gray-200 flex items-center rounded">
                 <img
                     src="https://purepng.com/public/uploads/large/purepng.com-flourflourgrainscerealbread-1411527418592jo6pj.png
                 "
@@ -13,7 +13,7 @@ const CartItem = () => {
                 <p className="font-bold text-lg">Flour</p>
                 <div className="flex space-x-2">
                     <p>Amount: </p>
-                    <input type="text" className="text-center w-6 bg-gray-200 rounded" />
+                    <input type="text" className="text-center w-6 bg-gray-200 rounded outline-none" />
                     <p>g</p>
                 </div>
                 <p>Price: $60</p>
